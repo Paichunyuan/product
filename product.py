6ad16c890c467ccc7baf13378cf80b747893309f
Bernie Pai <dges51101@gmail.com>
2020-11-30 14:38:51 +0800

    add check file

--- product.py
+++ product.py
@@ -1,11 +1,18 @@
+import os # operating system
+
 products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('有! 找到檔案了!')
+	# 讀取檔案
+	with open('products.csv', 'r') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('沒有找到檔案')
 
 # 讓使用者輸入
 products = []
